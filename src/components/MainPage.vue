8c0d008ed9f16132f0b1814e7680239c58388c04
Gracejean <jennilyn.mendoza@student.passerellesnumeriques.org>
2019-12-07 11:35:50 +0800

    dasfdA

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,7 +7,15 @@
           <div class="row">
             <div class="col-lg-3 col-md-3 col-sm-12 p-0">
               <div class="form-control search-slt">
-                <v-select v-model="search" :items="transpo" :rules="[v => !!v || 'Item is required']" label="Choose items" required></v-select>
+                <v-select
+                  v-model="select"
+                  :items="transpo"
+                  :rules="[v => !!v || 'Item is required']"
+                  label="Options"
+                  select
+                  return-object
+                  required
+                ></v-select>
               </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-12 p-0">
@@ -17,18 +25,30 @@
             </div>
             <div class="col-lg-3 col-md-3 col-sm-12 p-0">
               <div class="form-control search-slt">
-                <v-text-field type="date" v-model="dater" :prepend-icon="'mdi-calendar'" name="input-10-1" label="date reserved" />
+                <v-text-field
+                  type="date"
+                  v-model="dater"
+                  :prepend-icon="'mdi-calendar'"
+                  name="input-10-1"
+                  label="date reserved"
+                />
               </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-12 p-0">
               <div class="form-control search-slt">
-                <v-text-field label="date returned" type="date" v-model="date" :prepend-icon="'mdi-calendar'" name="input-10-1" />
+                <v-text-field
+                  label="date returned"
+                  type="date"
+                  v-model="date"
+                  :prepend-icon="'mdi-calendar'"
+                  name="input-10-1"
+                />
               </div>
             </div>
             <div class="col-lg-3 col-md-3 col-sm-12 p-0">
               <button type="button" class="btn btn-primary wrn-btn" @click="search()">
-                      <a>Search</a>
-                    </button>
+                <a>Search</a>
+              </button>
             </div>
           </div>
         </div>
@@ -45,9 +65,9 @@
             <div>
               <v-rating v-model="rating"></v-rating>
             </div>
-            <hr style="width:200px;height:5px;color:dodgerblue ">
+            <hr style="width:200px;height:5px;color:dodgerblue " />
             <v-card-title class="headline">Status:{{item.status}}</v-card-title>
-            <v-card-title class="headline">Location:{{item.location}}</v-card-title>
+            <v-card-title class="headline">Location:{{item.location1}}</v-card-title>
           </v-col>
           <div class="vertical"></div>
           <v-col class="col-3">
@@ -60,287 +80,311 @@
           <v-col class="col=6">
             <v-card-title class="company">Company:{{item.company}}</v-card-title>
             <v-card-title class="price">Price:{{item.price}}</v-card-title>
-            <v-btn class="primary" @click="reserved()">Reserved</v-btn>
+            <v-btn class="primary" v-on:click="reserved()">Reserved</v-btn>
           </v-col>
         </v-row>
       </v-container>
     </v-card>
-    <br>
-    <br>
-    <br>
+    <br />
+    <br />
+    <br />
     <Footer></Footer>
   </div>
 </template>
 <script>
-  import Header from "../components/Header.vue";
-  import Footer from "../components/Footer.vue";
-  export default {
-    components: {
-      Header,
-      Footer
-    },
-    data: () => ({
-      search: "",
-      items: [{
-          status: "available",
-          location: "Mandaue,City",
-          color: " Blue",
-          src: "https://images.dealer.com/ddc/vehicles/2019/Hyundai/Santa%20Fe%20XL/SUV/trim_SE_f0031a/color/Becketts%20Black-RB5-8%2C8%2C8-640-en_US.jpg",
-          itemname: "Hyundai XL HY01674",
-          price: "P1000 per day",
-          brand: " Hyundai ",
-          company: " Budget car  Rental",
-          sitcap: " 4 persons",
-          type: "car"
-        },
-        {
-          location: "Oslob,Cebu",
-          status: "available",
-          color: " orange",
-          src: "https://www.suzuki-zambia.com/media/gamme/modeles/graph/2524-photo-suzuki-alto-k10.png",
-          itemname: "Zuzuki Alto",
-          price: "P1500 per day",
-          brand: " zuzuki",
-          company: "Zuzuki Company",
-          sitcap: " 4 persons",
-          type: "car"
-        },
-        {
-          location: "Medellin Cebu",
-          status: "available",
-          color: " Black",
-          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQs7kaoUewSBHa7T6rUyWHNdqUuKnCwvuAAexV7xdkPy4w4a2D&s",
-          itemname: "Honda XRM 125 DS Fi",
-          price: "P1000 per day",
-          brand: " Honda",
-          company: " Motorbike rental ",
-          sitcap: " 2 persons",
-          type: "motorcycle"
-        },
-        {
-          location: "Ceby city",
-          status: "available",
-          color: "Black and Red",
-          src: "https://hondaph.com/wp-content/uploads/2019/09/XRM_MOTARD_RED.png",
-          itemname: "Yamaha FZi",
-          price: "P1500 per day",
-          brand: " Yamaha",
-          company: "Motorcycle rental ",
-          sitcap: " 2 persons",
-          type: "motorcycle"
-        },
-        {
-          location: "Mandaue,City",
-          status: "available",
-          color: "white",
-          src: "http://perthsuretaxi.com/images/ser-img3.jpg",
-          itemname: "Taxi Van",
-          price: "P2000 per day",
-          brand: " toyota",
-          company: " Rent a Van Cebu",
-          sitcap: " 8 persons",
-          type: "van"
-        },
-        {
-          location: "Cebu City",
-          status: "available",
-          color: " gray",
-          src: "http://toyota.com.ph/wp-content/uploads/2018/06/HH1303-F-01.png",
-          itemname: "Foton Trans Van",
-          price: "P1000 per day",
-          brand: " nissan",
-          company: "Mini Van Rental",
-          sitcap: " 8 persons",
-          type: "van"
-        },
-        {
-          location: "Mandaue,City",
-          status: "available",
-          color: " white",
-          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnVCmNE5VzQD8YWGKepaPZ6a0ioDKFVB2BDLuWGbZsQu43LR0c&s",
-          itemname: "Nathalie Tours",
-          price: "P3000 per day",
-          brand: " nathalie ",
-          company: "nathalie tours",
-          sitcap: "34 persons",
-          type: "bus"
-        },
-        {
-          location: "Oslob Cebu",
-          status: "available",
-          color: " white",
-          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM1_fR7ilTwIeH9KDiJDiznNEbT7XDA0v5E98o99689wFKWB5how&s",
-          itemname: "Catalina Bus Tour",
-          price: "P3500 per day",
-          brand: " Catalina",
-          company: "Catalina Bus Tours",
-          sitcap: "45 persons",
-          type: "bus"
-        },
-        {
-          location: "Medellin",
-          status: "available",
-          color: " white",
-          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8LkhXzxAXZj8qR4D6rM7QJYMeKtUPlQhzoriPu10GPv4mWOYg&s",
-          itemname: "Filipino Banka",
-          price: "P5000 per day",
-          brand: " none",
-          company: "Boat Design",
-          sitcap: " 8 persons",
-          type: "boat"
-        },
-        {
-          location: "Medellin",
-          status: "available",
-          color: " white",
-          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxfeR566GQF0vDZJvqq_JZp0ZQAZ-XzCuSkCYVlI717nN-79S&s",
-          itemname: "Adventure BAngka",
-          price: "P4000 per day",
-          brand: " none",
-          company: "Rise Above",
-          sitcap: " 8 persons",
-          type: "boat"
-        }
-      ],
-      filteredList() {
-        return this.items.filter(post => {
-          return post.type
-            .toLowerCase()
-            .includes(this.$route.params.filter.toLowerCase());
-        });
+import Header from "../components/Header.vue";
+// import Footer from "../components/Footer.vue";
+export default {
+  components: {
+    Header
+    // Footer
+  },
+  data: () => ({
+    select: "",
+    location: "",
+    valid: true,
+    items: [
+      {
+        status: "available",
+        location1: "Mandaue,City",
+        color: " Blue",
+        src:
+          "https://images.dealer.com/ddc/vehicles/2019/Hyundai/Santa%20Fe%20XL/SUV/trim_SE_f0031a/color/Becketts%20Black-RB5-8%2C8%2C8-640-en_US.jpg",
+        itemname: "Hyundai XL HY01674",
+        price: "P1000 per day",
+        brand: " Hyundai ",
+        company: " Budget car  Rental",
+        sitcap: " 4 persons",
+        type: "car"
       },
-      select: null,
-      transpo: ["car", "motorcycle", "van", "bus", "boat"],
-      date: new Date(),
-      dater: new Date(),
-      rating: 3
-    }),
-    methods: {
-      reserved() {
-        this.$router.push("/Login");
+      {
+        location1: "Oslob,Cebu",
+        status: "available",
+        color: " orange",
+        src:
+          "https://www.suzuki-zambia.com/media/gamme/modeles/graph/2524-photo-suzuki-alto-k10.png",
+        itemname: "Zuzuki Alto",
+        price: "P1500 per day",
+        brand: " zuzuki",
+        company: "Zuzuki Company",
+        sitcap: " 4 persons",
+        type: "car"
+      },
+      {
+        location1: "Medellin Cebu",
+        status: "available",
+        color: " Black",
+        src:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQs7kaoUewSBHa7T6rUyWHNdqUuKnCwvuAAexV7xdkPy4w4a2D&s",
+        itemname: "Honda XRM 125 DS Fi",
+        price: "P1000 per day",
+        brand: " Honda",
+        company: " Motorbike rental ",
+        sitcap: " 2 persons",
+        type: "motorcycle"
+      },
+      {
+        location1: "Ceby city",
+        status: "available",
+        color: "Black and Red",
+        src:
+          "https://hondaph.com/wp-content/uploads/2019/09/XRM_MOTARD_RED.png",
+        itemname: "Yamaha FZi",
+        price: "P1500 per day",
+        brand: " Yamaha",
+        company: "Motorcycle rental ",
+        sitcap: " 2 persons",
+        type: "motorcycle"
+      },
+      {
+        location1: "Mandaue,City",
+        status: "available",
+        color: "white",
+        src: "http://perthsuretaxi.com/images/ser-img3.jpg",
+        itemname: "Taxi Van",
+        price: "P2000 per day",
+        brand: " toyota",
+        company: " Rent a Van Cebu",
+        sitcap: " 8 persons",
+        type: "van"
+      },
+      {
+        location1: "Cebu City",
+        status: "available",
+        color: " gray",
+        src: "http://toyota.com.ph/wp-content/uploads/2018/06/HH1303-F-01.png",
+        itemname: "Foton Trans Van",
+        price: "P1000 per day",
+        brand: " nissan",
+        company: "Mini Van Rental",
+        sitcap: " 8 persons",
+        type: "van"
+      },
+      {
+        location1: "Mandaue,City",
+        status: "available",
+        color: " white",
+        src:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnVCmNE5VzQD8YWGKepaPZ6a0ioDKFVB2BDLuWGbZsQu43LR0c&s",
+        itemname: "Nathalie Tours",
+        price: "P3000 per day",
+        brand: " nathalie ",
+        company: "nathalie tours",
+        sitcap: "34 persons",
+        type: "bus"
+      },
+      {
+        location1: "Oslob Cebu",
+        status: "available",
+        color: " white",
+        src:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM1_fR7ilTwIeH9KDiJDiznNEbT7XDA0v5E98o99689wFKWB5how&s",
+        itemname: "Catalina Bus Tour",
+        price: "P3500 per day",
+        brand: " Catalina",
+        company: "Catalina Bus Tours",
+        sitcap: "45 persons",
+        type: "bus"
+      },
+      {
+        location1: "Medellin",
+        status: "available",
+        color: " white",
+        src:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8LkhXzxAXZj8qR4D6rM7QJYMeKtUPlQhzoriPu10GPv4mWOYg&s",
+        itemname: "Filipino Banka",
+        price: "P5000 per day",
+        brand: " none",
+        company: "Boat Design",
+        sitcap: " 8 persons",
+        type: "boat"
+      },
+      {
+        location1: "Medellin",
+        status: "available",
+        color: " white",
+        src:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgxfeR566GQF0vDZJvqq_JZp0ZQAZ-XzCuSkCYVlI717nN-79S&s",
+        itemname: "Adventure BAngka",
+        price: "P4000 per day",
+        brand: " none",
+        company: "Rise Above",
+        sitcap: " 8 persons",
+        type: "boat"
       }
-    },
-    mounted() {
-      this.items = this.items.filter(item => {
-        if (this.$route.params.filter) {
-          return (
-            item.type.toLowerCase().includes(this.$route.params.filter.toLowerCase()) 
-          );
-        } else {
-          return item
-        }
-      });
-    },
-    computed: {
-      itemsone() {
-        return this.$store.state.options.options;
-      }
-    },
+    ],
+    transpo: ["car", "motorcycle", "van", "bus", "boat"],
+    date: new Date(),
+    dater: new Date(),
+    rating: 3
+  }),
+  filteredList() {
+    return this.items.filter(post => {
+      return post.type
+        .toLowerCase()
+        .includes(this.$route.params.filter.toLowerCase());
+    });
+  },
+  methods: {
     search() {
       this.$router.push(`/mainpage/${this.select}`);
+      // this.filterSearch();
+    },
+    reserved() {
+      this.$router.push("/Login");
+    },
+    filterSearch() {
+      this.items = this.items.filter(item => {
+        if (this.$route.params.filter) {
+          return item.type
+            .toLowerCase()
+            .includes(this.$route.params.filter.toLowerCase());
+        } else {
+          return item;
+        }
+      });
     }
-  };
+  },
+  mounted() {
+    this.filterSearch();
+  },
+updated(){
+  this.filterSearch()
+},
+  filters: {
+    capitalize: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    }
+  },
+  computed: {
+    tranpo() {
+      return this.$store.state.options.options;
+    }
+  }
+};
 </script>
 <style scoped>
-  .title {
-    font-size: 40px;
-    font-family: 'Courier New', Courier, monospace
-  }
-  .vertical {
-    border-left: 1px solid grey;
-    height: 260px;
-    margin-left: -27%;
-    margin-top: 0.5%;
-  }
-  .verticalone {
-    border-left: 1px solid grey;
-    height: 260px;
-    margin-top: 0.5%;
-  }
-  .container {
-    width: 100%;
-    margin-left: 5%;
-    margin-right: 5%;
-  }
-  .row[data-v-109177d0] {
-    margin-right: -35px;
-    margin-left: -65px;
-    margin-top: -1px;
-  }
-  .row-one[data-v-109177d0][data-v-109177d0] {
-    margin-right: 12%;
-    margin-left: 15%;
-    margin-top: 5%;
-  }
-  .v-card[data-v-109177d0]:not(.v-sheet--tile):not(.v-card--shaped) {
-    border-radius: 4px sp;
-    width: 85%;
-    margin-top: 50px;
-    margin-left: 8%;
-    /* border: 2px solid dodgerblue; */
+.title {
+  font-size: 40px;
+  font-family: "Courier New", Courier, monospace;
+}
+.vertical {
+  border-left: 1px solid grey;
+  height: 260px;
+  margin-left: -27%;
+  margin-top: 0.5%;
+}
+.verticalone {
+  border-left: 1px solid grey;
+  height: 260px;
+  margin-top: 0.5%;
+}
+.container {
+  width: 100%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+.row[data-v-109177d0] {
+  margin-right: -35px;
+  margin-left: -65px;
+  margin-top: -1px;
+}
+.row-one[data-v-109177d0][data-v-109177d0] {
+  margin-right: 12%;
+  margin-left: 15%;
+  margin-top: 5%;
+}
+.v-card[data-v-109177d0]:not(.v-sheet--tile):not(.v-card--shaped) {
+  border-radius: 4px sp;
+  width: 85%;
+  margin-top: 50px;
+  margin-left: 8%;
+  /* border: 2px solid dodgerblue; */
 }
 .search-slt[data-v-109177d0][data-v-109177d0] {
-    width: 90%;
-    height: 70px;
-    font-family: Raleway-SemiBold;
-    /* border: 2px solid dodgerblue; */
+  width: 90%;
+  height: 70px;
+  font-family: Raleway-SemiBold;
+  /* border: 2px solid dodgerblue; */
 }
-  .theme--light.v-sheet {
-    background-color: #ffffff;
-    border-color: #1E90FF;
-    height: 290px;
-    color: rgba(0, 0, 0, 0.87);
-  }
-  .row {
-    margin-right: -12px;
-    margin-left: -12px;
-    margin-top: 50px;
-  }
-  .search-slt {
-    width: 90%;
-    height: 70px;
-    font-family: Raleway-SemiBold;
-  }
-  .datepicker {
-    font-size: 16px;
-    font-family: Raleway-SemiBold;
-  }
-  .col-lg-3 {
-    -ms-flex: 0 0 25%;
-    -webkit-box-flex: 0;
-    flex: 0 0 20%;
-    width: 5%;
-  }
-  .theme--light.v-select .v-select__selections {
-    color: rgba(0, 0, 0, 0.87);
-    margin-top: -20px;
-    font-size: 25px;
-  }
-  .wrn-btn {
-    font-size: 35px;
-    width: 200px;
-    height: 70px;
-    font-family: Raleway-SemiBold;
-  }
-  #head {
-    background-color: whitesmoke;
-  }
-  h1,
-  h2 {
-    font-size: 15px;
-  }
-  .company,
-  .price {
-    margin-left: 75px;
-  }
-  .primary {
-    margin-top: 50px;
-  }
-  .v-btn:not(.v-btn--round).v-size--default[data-v-109177d0] {
-    height: 56px;
-    min-width: 194px;
-    padding: 0 16px;
-    margin-left: 75px;
-    margin-top: 69px;
-  }
+.theme--light.v-sheet {
+  background-color: #ffffff;
+  border-color: #1e90ff;
+  height: 290px;
+  color: rgba(0, 0, 0, 0.87);
+}
+.row {
+  margin-right: -12px;
+  margin-left: -12px;
+  margin-top: 50px;
+}
+.search-slt {
+  width: 90%;
+  height: 70px;
+  font-family: Raleway-SemiBold;
+}
+.datepicker {
+  font-size: 16px;
+  font-family: Raleway-SemiBold;
+}
+.col-lg-3 {
+  -ms-flex: 0 0 25%;
+  -webkit-box-flex: 0;
+  flex: 0 0 20%;
+  width: 5%;
+}
+.theme--light.v-select .v-select__selections {
+  color: rgba(0, 0, 0, 0.87);
+  margin-top: -20px;
+  font-size: 25px;
+}
+.wrn-btn {
+  font-size: 35px;
+  width: 200px;
+  height: 70px;
+  font-family: Raleway-SemiBold;
+}
+#head {
+  background-color: whitesmoke;
+}
+h1,
+h2 {
+  font-size: 15px;
+}
+.company,
+.price {
+  margin-left: 75px;
+}
+.primary {
+  margin-top: 50px;
+}
+.v-btn:not(.v-btn--round).v-size--default[data-v-109177d0] {
+  height: 56px;
+  min-width: 194px;
+  padding: 0 16px;
+  margin-left: 75px;
+  margin-top: 69px;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,129 +1,102 @@
 <template>
-  <div class="container">
-    <v-card class="mx-auto my-12" max-width="374" @submit="onsubmit()">
-      <v-subheader height="200">
-        <h1>LOGIN TO ABANG</h1>
-      </v-subheader>
-      <v-card-text>
-        <v-row align="center" class="mx-0">
-          <v-text-field
-            v-model="username"
-            :type="'email' "
-            required
-            :prepend-icon="'mdi-account'"
-            name="input-10-1"
-            label="Username"
-          ></v-text-field>
-          <v-text-field
-            v-model="password"
-            required
-            :type="'password'"
-            :prepend-icon="'mdi-key-variant'"
-            name="input-10-1"
-            label="Password"
-          ></v-text-field>
-        </v-row>
-      </v-card-text>
-      <center>
-        <v-card-actions>
-          <v-btn id="customer" @click="onsubmit()">LOGIN</v-btn>
-          <!-- <v-btn id="company" >Company</v-btn> -->
-        </v-card-actions>
-      </center>
-      <v-card-actions>
-        <h2>
-          Dont have account? Click
-          <u>
-            <a @click="register">Register</a>
-          </u>
-        </h2>
-      </v-card-actions>
-    </v-card>
+<section class="login-block">
+    <div class="container">
+	<div class="row">
+		<div class="col-md-4 login-sec">
+		    <h2 class="text-center">Login Now</h2>
+		    <form class="login-form">
+  <div class="form-group">
+    <label for="exampleInputEmail1" class="text-uppercase">Username</label>
+    <input type="text" class="form-control" placeholder="">
+    
   </div>
-</template>
-<script>
-import AUTH from "../services/auth";
-import axios from "axios";
-export default {
-  name: "login",
-  // data: () => ({
-  //   input: {
-  //     username: "",
-  //     password: ""
-  //   },
-  //  // emailRules: [v => !!v || "username is required"],
-  //   // passwordRules: [
-  //   //   v => !!v || "Password is required",
-  //   //   v => (v && v.length >= 5) || "Password must have 5+ characters",
-  //   //   v => /(?=.*[A-Z])/.test(v) || "Must have one uppercase character",
-  //   //   v => /(?=.*\d)/.test(v) || "Must have one number",
-  //   //   v => /([!@$%])/.test(v) || "Must have one special character [!@#$%]"
-  //   // ]
-  // }),
-  data() {
-    return{
-      username: "",
-      password: ""
-    }
-  },
-  methods: {
-    onsubmit() {
-     // e.preventDefault();
-      // let user = AUTH.login(this.input.username, this.input.password);
-      // if (this.input.username === "" || this.input.password === "") {
-        var data = {
-          username: this.username,
-          password: this.password
-        }
-        axios.get("http://localhost:5000/login", data)
-          .then(response => {
-            console.log(response);
-            this.data = "";
-            this.$router.push("/mainpage");
-          })
-          .catch(err => {
-            alert("Username or Password doesn't match")
-            console.log(err)
-          })
-      // } else {
-      //   AUTH.setUser(user);
-      //   if (user !== null) {
-      //     Router.push("/reserved");
-      //   }
-      // }
-    },
-    register() {
-      this.$router.push("/register");
-    }
-  }
-};
-</script>
-<style scoped>
-.theme--light.v-subheader {
-  background-color: #1976d2;
-  height: 100px;
-}
-.btn {
-  margin-left: 30px;
-}
-.v-card__actions .v-btn.v-btn {
-  padding: 0 8px;
-  background-color: #1976d2;
-  margin-left: 109px;
-  width: 135px;
-}
-#customer,
-#company {
-  margin-left: 8%;
-}
-h2 {
-  font-size: 15px;
-  margin-left: 20%;
-}
-h1{
-  font-size: 20px;
-  color: aliceblue;
-  font-family: 'Franklin Gothic Medium'
+  <div class="form-group">
+    <label for="exampleInputPassword1" class="text-uppercase">Password</label>
+    <input type="password" class="form-control" placeholder="">
+  </div>
   
+  
+    <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      <small>Remember Me</small>
+    </label>
+    <button type="submit" class="btn btn-login float-right">Submit</button>
+  </div><br><br>
+  <div>
+    <h1>Don't have an account? Click <a>Register!!..</a></h1>
+  </div>
+
+  
+</form>
+<div class="copy-text">Created with <i class="fa fa-heart"></i> by Grafreez</div>
+		</div>
+		<div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                 <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+            <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>	
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>	
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block img-fluid" src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="banner-text">
+            <h2>This is Heaven</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+        </div>	
+    </div>
+  </div>
+            </div>	   
+		    
+		</div>
+	</div>
+</div>
+    </div>
+</section>
+</template>
+<style scoped>
+@import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+.login-block{
+background: #DE6262;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #FFB88C, #DE6262);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #FFB88C, #DE6262); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+float:left;
+width:100%;
+padding : 50px 0;
 }
+/* .banner-sec{background:url(https://static.pexels.com/photos/33972/pexels-photo.jpg)  no-repeat left bottom; background-size:cover; min-height:500px; border-radius: 0 10px 10px 0; padding:0;} */
+.container{background:#fff; border-radius: 10px; box-shadow:15px 20px 0px rgba(0,0,0,0.1);}
+.carousel-inner{border-radius:0 10px 10px 0;}
+.carousel-caption{text-align:left; left:5%;}
+.login-sec{padding: 50px 30px; position:relative;}
+.login-sec .copy-text{position:absolute; width:80%; bottom:20px; font-size:13px; text-align:center;}
+.login-sec .copy-text i{color:rgb(63, 124, 238);}
+.login-sec .copy-text a{color:rgb(74, 163, 204);}
+.login-sec h2{margin-bottom:30px; font-weight:800; font-size:30px; color: #DE6262;}
+.login-sec h2:after{content:" "; width:100px; height:5px; background:#FEB58A; display:block; margin-top:20px; border-radius:3px; margin-left:auto;margin-right:auto}
+.btn-login{background: #DE6262; color:#fff; font-weight:600;}
+/* .banner-text{width:70%; position:absolute; bottom:40px; padding-left:20px;}
+.banner-text h2{color:#fff; font-weight:600;}
+.banner-text h2:after{content:" "; width:100px; height:5px; background:#FFF; display:block; margin-top:20px; border-radius:3px;}
+.banner-text p{color:#fff;} */
 </style>
