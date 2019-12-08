a46ac67450aa3b44fe5b2d8a6e9f0f216752aaa5
jorgielyn <irangabriellef14@gmail.com>
2019-12-08 22:07:40 +0800

    added

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4 login-sec">
-          <h2 class="text-center">Login Now</h2>
+          <h2 class="text-center">Register</h2>
           <form class="login-form">
             <v-text-field v-model="data.firstname" label="Firtsname" required></v-text-field>
             <v-text-field v-model="data.lastname" label="Lastname" required></v-text-field>
@@ -30,66 +30,26 @@
             <br>
             <br>
             <div>
-              <h1>
-                Don't have an account? Click
-                <a @click="register">
-                  <u>Register!!..</u>
-                </a>
-              </h1>
             </div>
           </form>
-          <div class="copy-text">
-            Created with
-            <i class="fa fa-heart"></i> by Grafreez
-          </div>
         </div>
         <div class="col-md-8 banner-sec">
           <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
             <div class="carousel-inner" role="listbox">
               <div class="carousel-item active">
                 <img
                   class="d-block img-fluid"
-                  src="https://static.pexels.com/photos/33972/pexels-photo.jpg"
+                  src="https://media.licdn.com/dms/image/C561BAQFj57SmFUgXew/company-background_10000/0?e=2159024400&v=beta&t=s0P5tQcgs3u14sD7wNdISUOgxGXd6B9Gcrz1fIfm990"
                   alt="First slide"
                 >
                 <div class="carousel-caption d-none d-md-block">
                   <div class="banner-text">
-                    <h2>This is Heaven</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                    <h2>ABANG</h2>
+                    <p>We Keep You Moving</p>
                   </div>
                 </div>
               </div>
-              <div class="carousel-item">
-                <img
-                  class="d-block img-fluid"
-                  src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg"
-                  alt="First slide"
-                >
-                <div class="carousel-caption d-none d-md-block">
-                  <div class="banner-text">
-                    <h2>This is Heaven</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-                  </div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img
-                  class="d-block img-fluid"
-                  src="https://images.pexels.com/photos/872957/pexels-photo-872957.jpeg"
-                  alt="First slide"
-                >
-                <div class="carousel-caption d-none d-md-block">
-                  <div class="banner-text">
-                    <h2>This is Heaven</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-                  </div>
-                </div>
-              </div>
+             
             </div>
           </div>
         </div>
@@ -100,16 +60,16 @@
 <style scoped>
 @import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
 .login-block {
-  background: #de6262; /* fallback for old browsers */
+  background: #1E90FF; /* fallback for old browsers */
   background: -webkit-linear-gradient(
     to bottom,
-    #ffb88c,
-    #de6262
+    #31a0cc,
+    #405bcf
   ); /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(
     to bottom,
-    #ffb88c,
-    #de6262
+    #31a0cc,
+    #405bcf
   ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   float: left;
   width: 100%;
